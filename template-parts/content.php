c60196c55f807b1b2cf104a110590641af007ad7
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2023-10-24 13:26:27 +0300

    added template-parts

---
+++ template-parts/content.php
@@ -0,0 +1,25 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <div class="entry-content">
+        <?php
+            the_content( sprintf(
+                wp_kses( 
+                    __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'mir'),
+                    array(
+                        'span' => array(
+                            'class' => array(),
+                        ),
+                    )
+                ),
+                get_the_title()
+            ));
+
+            wp_link_pages( array(
+                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mir' ),
+                'after'  => '</div>',
+            ) );
+        ?>
+
+    </div>
+
+</article>
